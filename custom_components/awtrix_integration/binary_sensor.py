25bcfa45c10236a1beb9a791383eebab6b2cd064
Stephan <31169771+Blueforcer@users.noreply.github.com>
2024-03-10 01:33:20 +0100

    renaming

--- custom_components/awtrix_integration/binary_sensor.py
+++ custom_components/awtrix_integration/binary_sensor.py
@@ -1,4 +1,4 @@
-"""Binary sensor platform for integration_blueprint."""
+"""Binary sensor platform for awtrix_integration."""
 from __future__ import annotations
 
 from homeassistant.components.binary_sensor import (
@@ -13,8 +13,8 @@ from .entity import IntegrationBlueprintEntity
 
 ENTITY_DESCRIPTIONS = (
     BinarySensorEntityDescription(
-        key="integration_blueprint",
-        name="Integration Blueprint Binary Sensor",
+        key="awtrix_integration",
+        name="AWTRIX Integration Binary Sensor",
         device_class=BinarySensorDeviceClass.CONNECTIVITY,
     ),
 )
@@ -33,7 +33,7 @@ async def async_setup_entry(hass, entry, async_add_devices):
 
 
 class IntegrationBlueprintBinarySensor(IntegrationBlueprintEntity, BinarySensorEntity):
-    """integration_blueprint binary_sensor class."""
+    """awtrix_integration binary_sensor class."""
 
     def __init__(
         self,
--- custom_components/awtrix_integration/const.py
+++ custom_components/awtrix_integration/const.py
@@ -1,9 +1,9 @@
-"""Constants for integration_blueprint."""
+"""Constants for awtrix_integration."""
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+NAME = "AWTRIX Integration"
+DOMAIN = "awtrix_integration"
 VERSION = "0.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
